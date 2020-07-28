de8e6e0a9ade5d13b5171589b13699158e743ccf
barunslick <barunpradhan987@gmail.com>
2020-07-28 10:55:55 +0545

    Updates

---
+++ precache-manifest.4b969d1d265c4273871041850b23550b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4983beed97c0580b8ee9e53c8bd3841d",
+    "url": "/react-intro/index.html"
+  },
+  {
+    "revision": "1514eaab637914990cb1",
+    "url": "/react-intro/static/css/main.65759a6e.chunk.css"
+  },
+  {
+    "revision": "a82c789e4a99d29bd732",
+    "url": "/react-intro/static/js/2.57cf4e78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-intro/static/js/2.57cf4e78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1514eaab637914990cb1",
+    "url": "/react-intro/static/js/main.c15b297c.chunk.js"
+  },
+  {
+    "revision": "bbabea2acd9d0d43c754",
+    "url": "/react-intro/static/js/runtime-main.4a178d3f.js"
+  },
+  {
+    "revision": "60dceeba7df9e4c31fa0d368bd54009b",
+    "url": "/react-intro/static/media/author.60dceeba.jpg"
+  }
+]);
